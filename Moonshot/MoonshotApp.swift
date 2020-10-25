be9897dd5f76c04773d4eab46ba1774216baa0d3
Dobuzi <kimjw7981@gmail.com>
2020-10-25 11:22:31 +0900

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by 김종원 on 2020/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
